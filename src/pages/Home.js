9d2549a080994790c4612f7f0ef29a9fc38d9cd6
mika1407 <koneautomaatio@hotmail.com>
2021-04-24 14:12:52 +0300

    Pet.js sivu lisätty

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,59 +1,13 @@
 import PropTypes from "prop-types";
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import Pet from "./Pet"
 
-
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 200,
-  },
-});
 
 // const Home = ({text}) => {
 //   return <div>{text}</div>
 // }
 
 const Home = ({pets}) => {
-
-  const classes = useStyles();
-
-  return (
-      pets.map((pet) => 
-    <Card className={classes.root} key={pet.id}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={pet.img}
-          title="Pet"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {pet.name}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {pet.age}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Delete
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>)
-  );
+  return pets.map((pet) => <Pet pet={pet} key={pet.id} />);
 }
 // Home.defaultProps = {
 //   text: "Home page"
---
+++ src/pages/Pet.js
@@ -0,0 +1,52 @@
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 200,
+  },
+});
+
+const Pet = ({pet}) => {
+   const classes = useStyles();
+
+  return (
+  <Card className={classes.root} key={pet.id}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={pet.img}
+          title="Pet"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {pet.name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {pet.age}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Delete
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  )
+}
+
+export default Pet
