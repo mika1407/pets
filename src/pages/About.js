14dfa59862d3947cc35216870856780c33a590a1
mika1407 <koneautomaatio@hotmail.com>
2021-04-26 17:31:40 +0300

    vika

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ const About = () => {
   return (
     <div>
       <br/>
-     <h1 style={{ marginLeft: '1.5rem' }}>These pages are About pets.</h1>  <br/>
+     <h1 style={{ marginLeft: '1.5rem' }}>These pages are about pets.</h1>  <br/>
       <p style={{ marginLeft: '1.5rem' }}>Dod image urls you can find here: <a target="_blank" href="https://dog.ceo/dog-api/">Dod.ceo</a></p>
       <p style={{ marginLeft: '1.5rem' }}>YouTube video of these websites: <a target="_blank" href="https://www.youtube.com/watch?v=-sld1zTz3uY">Link to YouTube</a></p>
       <br/>
